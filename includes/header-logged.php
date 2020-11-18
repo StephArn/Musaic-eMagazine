b112895e79a0fb6c6e22fe8b39f47f15a8f15dd1
StephArn <34690734+StephArn@users.noreply.github.com>
2020-11-19 00:12:00 +0200

    Add files via upload

---
+++ includes/header-logged.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link href='https://fonts.googleapis.com/css?family=Dancing Script' rel='stylesheet'>
+    <link rel="stylesheet" href="/includes/header.css">
+    <title>PHP Test 2</title>
+</head>
+<body>
+	<div class="overlay">
+		<h1>Musaic</h1>
+		<br>
+		<nav>
+				<a class="header_link"  href="home-logged.php">Home</a>
+				<!--<a class="header_link" href="logout.php">Logout</a>
+				<a class="header_link" href="reset-password.php">Reset Password</a>
+				-->
+				<a class="header_link"  href="profile.php">Profile</a>
+				<a class="header_link" href="news.php">News</a>
+				<a class="header_link" href="interviews.php">Interviews</a>
+				<a class="header_link" href="reviews.php">Reviews</a>
+				<a class="header_link" href="playlists.php">Playlists</a>
+				<a class="header_link" href="gallery.php">Gallery</a>
+		</nav>
+		</div>
+</body>
+</html>
