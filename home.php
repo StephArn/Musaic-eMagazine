946c0d9ff6d95e2acadea03239973ab9c85b60bd
StephArn <34690734+StephArn@users.noreply.github.com>
2020-11-19 00:15:02 +0200

    Add files via upload

---
+++ home.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en-US">
+
+<head>
+<meta charset="UTF-8">
+<meta name="description" content="proiect PHP">
+<meta name="keywords" content="PHP, Music Magazine, Music Review, Music Interview, Music Tutorials">
+<meta name="author" content="Stefana Arina Tabusca">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+	<link href='https://fonts.googleapis.com/css?family=Cherry Swash' rel='stylesheet'><title> Musaic Magazine </title>
+    <link href='https://fonts.googleapis.com/css?family=Emblema One' rel='stylesheet'>
+	<link rel="stylesheet" type="text/css" href="home.css">
+	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<style>
+	body,h1,h2,h3,h4,h5,h6 {font-family: "Lato", sans-serif;}
+	.w3-bar,h1,button {font-family: "Montserrat", sans-serif}
+	.fa-anchor,.fa-coffee {size:200px;}
+	</style>
+<body>
+	
+
+</head>
+<body>
+<header>
+	<?php
+        include('includes/header.php');
+    ?>
+	</header>
+	<!-- First Grid -->
+<div class="w3-row-padding w3-padding-64 w3-container">
+  <div class="w3-content">
+    <div class="w3-twothird">
+      <h1>Lorem Ipsum</h1>
+      <h5 class="w3-padding-32">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</h5>
+
+      <p class="w3-text-grey">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+        laboris nisi ut aliquip ex ea commodo consequat.</p>
+    </div>
+
+    <div class="w3-third w3-center">
+	<img src="img\music-pngrepo-com.png" alt="Music Note">
+      <!--<i class="fa fa-anchor w3-padding-64 w3-text-red"></i> -->
+    </div>
+  </div>
+</div>
+
+<!-- Second Grid -->
+<div class="w3-row-padding w3-light-grey w3-padding-64 w3-container">
+  <div class="w3-content">
+    <!--<div class="w3-third w3-center">
+      <img src="img\music-pngrepo-com.png" alt="Music Note">
+    </div>
+-->
+    <div class="w3-twothird">
+      <h1>Lorem Ipsum</h1>
+      <h5 class="w3-padding-32">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</h5>
+
+      <p class="w3-text-grey">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+        laboris nisi ut aliquip ex ea commodo consequat.</p>
+    </div>
+	<div class="w3-third w3-center">
+	<img src="img\music-pngrepo-com.png" alt="Music Note">
+      <!--<i class="fa fa-anchor w3-padding-64 w3-text-red"></i> -->
+    </div>
+  </div>
+</div>
+
+<div class="w3-container w3-black w3-center w3-opacity w3-padding-64">
+    <h1 class="w3-margin w3-xlarge">Quote of the day: Music is life itself.</h1>
+</div>
+	<footer>
+<?php
+        include('includes/footer.php');
+    ?>
+	</footer>
+</body>
+<html>
---
+++ profile.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en-US">
+
+<head>
+<meta charset="UTF-8">
+<meta name="description" content="proiect PHP">
+<meta name="keywords" content="PHP, Music Magazine, Music Review, Music Interview, Music Tutorials">
+<meta name="author" content="Stefana Arina Tabusca">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+	<link href='https://fonts.googleapis.com/css?family=Cherry Swash' rel='stylesheet'><title> Musaic Magazine </title>
+    <link href='https://fonts.googleapis.com/css?family=Emblema One' rel='stylesheet'>
+	<link rel="stylesheet" type="text/css" href="home-logged.css">
+
+<body>
+	
+
+</head>
+<body>
+<header>
+	<?php
+        include('includes/header-logged.php');
+    ?>
+	</header>
+	
+	<button ><a href="reset-password.php">Reset your password</a></button>
+    <button ><a href="logout.php">Logout</a></button>
+	<button ><a href="acc-details.php">Account Details</a></button>
+
+<?php
+        include('includes/footer.php');
+    ?>
+	</footer>
+</body>
+<html>
