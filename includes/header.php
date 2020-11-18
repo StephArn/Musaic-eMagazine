102132d15490095a5defdd2f798aa9c40f5bc5d1
StephArn <34690734+StephArn@users.noreply.github.com>
2020-11-19 00:11:27 +0200

    Add files via upload

---
+++ includes/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link href='https://fonts.googleapis.com/css?family=Dancing Script' rel='stylesheet'>
+    <link rel="stylesheet" href="/includes/header.css">
+    <title>PHP Test 2</title>
+</head>
+<body>
+	<div class="overlay">
+		<h1>Musaic</h1>
+		<br>
+		<nav>
+				<a class="header_link"  href="home.php">Home</a>
+				<a class="header_link" href="register.php">Register</a>
+                <a class="header_link" href="login.php">Login</a>
+				<a class="header_link" href="news.php">News</a>
+				<a class="header_link" href="interviews.php">Interviews</a>
+				<a class="header_link" href="reviews.php">Reviews</a>
+				<a class="header_link" href="playlists.php">Playlists</a>
+				<a class="header_link" href="gallery.php">Gallery</a>
+		</nav>
+		</div>
+</body>
+</html>
