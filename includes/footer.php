2f134e7b5124509e8c009be7f47c0ed2bbb680f3
StephArn <34690734+StephArn@users.noreply.github.com>
2020-11-19 00:12:22 +0200

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link href='https://fonts.googleapis.com/css?family=Dancing Script' rel='stylesheet'>
+    <link rel="stylesheet" href="/includes/header.css">
+    <title>PHP Test 2</title>
+</head>
+<body>
+    <header>
+        <h1 class="footer">Contact us:</h1>
+    </header>
+</body>
+</html>
