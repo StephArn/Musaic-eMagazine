04044fd4d1c63b4148188bd467fd3767ad283847
StephArn <34690734+StephArn@users.noreply.github.com>
2020-12-10 00:59:19 +0200

    Add files via upload

---
+++ delete_playlist.php
@@ -0,0 +1,96 @@
+<?php
+session_start();
+// Process delete operation after confirmation
+if(isset($_POST["id"]) && !empty($_POST["id"])){
+    // Include config file
+    require_once "config.php";
+    require_once 'token.php';
+    if($_SESSION['agent'] != $_SERVER['HTTP_USER_AGENT']) {
+    die('Session MAY have been hijacked');
+}
+    $csrf_token = $_POST['csrf_token'];
+     if(!Token::check_token($csrf_token))
+		{
+          die('Invalid token.');
+        }
+    // Prepare a delete statement
+    $sql = "DELETE FROM playlist WHERE playlist_id = ?";
+    
+    if($stmt = mysqli_prepare($link, $sql)){
+        // Bind variables to the prepared statement as parameters
+        mysqli_stmt_bind_param($stmt, "i", $param_id);
+        
+        // Set parameters
+        $param_id = trim($_POST["id"]);
+        
+        // Attempt to execute the prepared statement
+        if(mysqli_stmt_execute($stmt)){
+            // Records deleted successfully. Redirect to landing page
+            header("location: home-logged_m.php");
+            exit();
+        } else{
+            echo "Oops! Something went wrong. Please try again later.";
+        }
+    }
+     
+    // Close statement
+    mysqli_stmt_close($stmt);
+    
+    // Close connection
+    mysqli_close($link);
+} else{
+    // Check existence of id parameter
+    if(empty(trim($_GET["id"]))){
+        // URL doesn't contain id parameter. Redirect to error page
+        header("location: error.php");
+        exit();
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>View Account</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script src="https://code.jquery.com/jquery-1.11.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/jquery.validation/1.15.0/jquery.validate.min.js"></script>
+<script src="https://cdn.jsdelivr.net/jquery.validation/1.15.0/additional-methods.min.js"></script>
+
+    <style type="text/css">
+        .wrapper{
+            width: 500px;
+            margin: 0 auto;
+        }
+    </style>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="page-header">
+                        <h1>Delete Playlist</h1>
+                    </div>
+                    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                        <div class="alert alert-danger fade in">
+                            <input type="hidden" name="id" value="<?php echo trim($_GET["id"]); ?>"/>
+                            <p>Are you sure you want to delete this account?</p><br>
+                                                        <div>
+<input id="login-username" type="hidden" class="form-control" name="csrf_token" value="<?php  echo $_SESSION["token"];  ?>">  
+</div>
+                            <p>
+
+                                <input type="submit" value="Yes" class="btn btn-danger">
+                                <a href="acc-details.php" class="btn btn-default">No</a>
+                            </p>
+                        </div>
+                    </form>
+                </div>
+            </div>        
+        </div>
+    </div>
+</body>
+</html>
