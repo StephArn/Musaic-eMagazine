ea42312706725848467c0afe84f6ab05bcebbc52
StephArn <34690734+StephArn@users.noreply.github.com>
2020-12-10 00:53:35 +0200

    Update header-logged.php

--- includes/header-logged.php
+++ includes/header-logged.php
@@ -1,3 +1,29 @@
+<?php
+session_start();
+ 
+// Check if the user is logged in, otherwise redirect to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
+require_once "config.php";
+
+$id = $_SESSION["user_id"];
+
+$r_query = "SELECT role FROM user WHERE user_id = ".$id;  
+$role = mysqli_query($link, $r_query);
+$row = $role->fetch_assoc();
+$role= $row['role']; 
+if ($role == "basic") 
+$r ="home-logged_b.php";
+elseif ($role == "amateur")
+$r ="home-logged_a.php";
+elseif($role == "pro")
+$r ="home-logged_p.php";
+elseif ($role == "mod")
+$r ="home-logged_m.php";             
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,6 +31,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<link href='https://fonts.googleapis.com/css?family=Dancing Script' rel='stylesheet'>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+	<style>
+	button {font-family: "Montserrat", sans-serif}
+	</style>
     <link rel="stylesheet" href="/includes/header.css">
     <title>PHP Test 2</title>
 </head>
@@ -13,16 +43,19 @@
 		<h1>Musaic</h1>
 		<br>
 		<nav>
-				<a class="header_link"  href="home-logged.php">Home</a>
-				<!--<a class="header_link" href="logout.php">Logout</a>
-				<a class="header_link" href="reset-password.php">Reset Password</a>
-				-->
-				<a class="header_link"  href="profile.php">Profile</a>
-				<a class="header_link" href="news.php">News</a>
-				<a class="header_link" href="interviews.php">Interviews</a>
-				<a class="header_link" href="reviews.php">Reviews</a>
-				<a class="header_link" href="playlists.php">Playlists</a>
-				<a class="header_link" href="gallery.php">Gallery</a>
+
+                <a  href="<?php echo $r ?>"><button  class = "header_link" >Home </button></a>
+				<a  href="profile.php"> <button  class = "header_link" >Profile </button></a>
+				<a href="news.php"><button  class = "header_link" >News </button></a>
+				<a href="interviews.php"><button  class = "header_link" >Interviews </button></a>
+				<a href="reviews.php"><button  class = "header_link" >Reviews </button></a>
+				<a href=" <?php
+                            if ($_SESSION["role"] == "mod")
+                                echo "playlist_m.php";
+                            else echo "playlists.php";
+                             ?> 
+                             "><button  class = "header_link" >Playlists </button></a>
+				<a href="gallery.php"><button  class = "header_link" >Gallery </button></a>
 		</nav>
 		</div>
 </body>
