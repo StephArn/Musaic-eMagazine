b09ed1d476c818cacfac1a3638e2c1e8d54efe9d
StephArn <34690734+StephArn@users.noreply.github.com>
2020-12-10 00:55:14 +0200

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -5,6 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<link href='https://fonts.googleapis.com/css?family=Dancing Script' rel='stylesheet'>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+	<style>
+	button {font-family: "Montserrat", sans-serif}
+	</style>
     <link rel="stylesheet" href="/includes/header.css">
     <title>PHP Test 2</title>
 </head>
@@ -13,14 +17,14 @@
 		<h1>Musaic</h1>
 		<br>
 		<nav>
-				<a class="header_link"  href="home.php">Home</a>
-				<a class="header_link" href="register.php">Register</a>
-                <a class="header_link" href="login.php">Login</a>
-				<a class="header_link" href="news.php">News</a>
-				<a class="header_link" href="interviews.php">Interviews</a>
-				<a class="header_link" href="reviews.php">Reviews</a>
-				<a class="header_link" href="playlists.php">Playlists</a>
-				<a class="header_link" href="gallery.php">Gallery</a>
+				<a href="home.php"><button  class = "header_link" >Home </button></a>
+				<a href="register.php"><button  class = "header_link" >Register </button> </a>
+                <a href="login.php"><button  class = "header_link" >Login</button></a>
+				<a href="news.php"><button  class = "header_link" >News</button></a>
+				<a href="interviews.php"><button  class = "header_link" >Interviews</button></a>
+				<a href="reviews.php"><button  class = "header_link" >Reviews</button></a>
+				<a href="playlists.php"><button  class = "header_link" >Playlists</button></a>
+				<a href="gallery.php"><button  class = "header_link" >Gallery </button></a>
 		</nav>
 		</div>
 </body>
