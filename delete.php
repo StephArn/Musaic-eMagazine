c26f16c2d1c65a6075b4b2c7c031643efdb3d70c
StephArn <34690734+StephArn@users.noreply.github.com>
2020-12-10 00:58:43 +0200

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,9 +1,18 @@
 <?php
+session_start();
 // Process delete operation after confirmation
 if(isset($_POST["id"]) && !empty($_POST["id"])){
     // Include config file
     require_once "config.php";
-    
+    require_once 'token.php';
+    if($_SESSION['agent'] != $_SERVER['HTTP_USER_AGENT']) {
+    die('Session MAY have been hijacked');
+}
+    $csrf_token = $_POST['csrf_token'];
+     if(!Token::check_token($csrf_token))
+		{
+          die('Invalid token.');
+        }
     // Prepare a delete statement
     $sql = "DELETE FROM user WHERE user_id = ?";
     
@@ -44,6 +53,12 @@ if(isset($_POST["id"]) && !empty($_POST["id"])){
     <meta charset="UTF-8">
     <title>View Account</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script src="https://code.jquery.com/jquery-1.11.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/jquery.validation/1.15.0/jquery.validate.min.js"></script>
+<script src="https://cdn.jsdelivr.net/jquery.validation/1.15.0/additional-methods.min.js"></script>
+
     <style type="text/css">
         .wrapper{
             width: 500px;
@@ -52,11 +67,6 @@ if(isset($_POST["id"]) && !empty($_POST["id"])){
     </style>
 </head>
 <body>
-<header>
-	<?php
-        include('includes/header-logged.php');
-    ?>
-	</header>
     <div class="wrapper">
         <div class="container-fluid">
             <div class="row">
@@ -68,7 +78,11 @@ if(isset($_POST["id"]) && !empty($_POST["id"])){
                         <div class="alert alert-danger fade in">
                             <input type="hidden" name="id" value="<?php echo trim($_GET["id"]); ?>"/>
                             <p>Are you sure you want to delete this account?</p><br>
+                                                        <div>
+<input id="login-username" type="hidden" class="form-control" name="csrf_token" value="<?php  echo $_SESSION["token"];  ?>">  
+</div>
                             <p>
+
                                 <input type="submit" value="Yes" class="btn btn-danger">
                                 <a href="acc-details.php" class="btn btn-default">No</a>
                             </p>
@@ -78,10 +92,5 @@ if(isset($_POST["id"]) && !empty($_POST["id"])){
             </div>        
         </div>
     </div>
-	<footer>
-<?php
-        include('includes/footer.php');
-    ?>
-	</footer> 
 </body>
 </html>
