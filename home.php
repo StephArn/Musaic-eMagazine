7c0243d2587b93996280b88f85dd789684d17d46
StephArn <34690734+StephArn@users.noreply.github.com>
2020-12-10 01:04:23 +0200

    Update home.php

--- home.php
+++ home.php
@@ -1,3 +1,17 @@
+<?php
+session_start();
+if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+     if ($_SESSION["role"] == "basic")
+        header("location: home-logged_b.php");
+    elseif ($_SESSION["role"] == "amateur")
+        header("location: home-logged_a.php");
+                            elseif ($_SESSION["role"] == "pro")
+                            header("location: home-logged_p.php");
+                            elseif ($_SESSION["role"] == "mod")
+                            header("location: home-logged_m.php");
+  exit;
+}
+?>
 <!DOCTYPE html>
 <html lang="en-US">
 
@@ -14,6 +28,7 @@
 	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<style>
 	body,h1,h2,h3,h4,h5,h6 {font-family: "Lato", sans-serif;}
